7fd95cc527138799cba6a96fc794b5500a94d8a4
Jeffrey Wilges <jeffrey@wilges.com>
2021-04-13 21:02:13 -0700

    Include build tools in Nox environment; update frozen dependencies

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,12 @@
 import nox
 
-nox.options.sessions = ['dev-3.8']
+nox.options.sessions = ['dev-3.9']
 nox.options.error_on_missing_interpreters = True
 
 
-@nox.session(python=['3.6', '3.7', '3.8'])
+@nox.session(python=['3.6', '3.7', '3.8', '3.9'])
 def dev(session):
+    session.install('-r', 'requirements.build.txt')
     session.install('-r', 'requirements.dev.txt')
     session.install('-c', 'requirements.txt', '-e', '.')
     if not session.posargs:
